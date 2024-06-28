5dae97e6c81b4f4b9c7daf58093510aa85c8c63c
Daniel Enesi <137162024+DanielOnGitHub17@users.noreply.github.com>
2024-06-28 12:39:24 +0000

    render

--- frontend/views.py
+++ frontend/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse, Http404
 # from share.forms import InterviewForm
 
 def app_view(request):
-    return HttpResponse("Testing AI Interview")
+    # return HttpResponse("Testing AI Interview")
     context = {
         # "share_form": InterviewForm(),
     }
