0d8a0e3c7a7d3583edfe0a6df54b728f7eda9e2e
Daniel Enesi <137162024+DanielOnGitHub17@users.noreply.github.com>
2024-06-28 18:06:46 +0000

    specified python versions as 3.12

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,4 @@
-#!/usr/bin/env python3.12
 pip install -r requirements.txt
-python manage.py collectstatic
-python manage.py makemigrations
-python manage.py migrate
+python3.12 manage.py collectstatic
+python3.12 manage.py makemigrations
+python3.12 manage.py migrate
