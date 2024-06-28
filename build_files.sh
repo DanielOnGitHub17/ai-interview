7dcf3369b4202e33abb371b27291de35e5cd1b71
Daniel Enesi <137162024+DanielOnGitHub17@users.noreply.github.com>
2024-06-28 18:09:23 +0000

    removed specification

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,4 @@
 pip install -r requirements.txt
-python3.12 manage.py collectstatic
-python3.12 manage.py makemigrations
-python3.12 manage.py migrate
+python manage.py collectstatic
+python manage.py makemigrations
+python manage.py migrate
