00c6ec28b1c30a1738b7666e2244a99cb265403a
Daniel Enesi <137162024+DanielOnGitHub17@users.noreply.github.com>
2024-06-29 00:59:26 +0000

    Overrode model save method and defined config

--- ai/models.py
+++ ai/models.py
@@ -4,5 +4,11 @@ from django.db import models
 
 class ConfigDetails(models.Model):
     # {"last_used": 1719585805.504833, "usage": 14}
+    # I created an instance on the server and overrode the save method :)
     last_used = models.FloatField(default=0)
     usage = models.IntegerField()
+
+    def save(self):
+        pass
+
+config = ConfigDetails.objects.all()[0]  # Will change on Gemini query
