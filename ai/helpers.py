974badc1200c07800e367ce69b8d07f6d0e2b8b3
Daniel Enesi <137162024+DanielOnGitHub17@users.noreply.github.com>
2024-06-29 07:50:31 +0000

    Bad practice to query database on initialization. Also overrode the save method well.
    AI now has output 'cleaner'

--- ai/helpers.py
+++ ai/helpers.py
@@ -2,7 +2,7 @@ import google.generativeai as genai
 import json
 import os
 
-from .models import config
+from .models import ConfigDetails
 from datetime import datetime
 
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
@@ -10,67 +10,52 @@ genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 interview_model = genai.GenerativeModel("gemini-pro")
 more_queries = "your response should be straightforward. "\
                 "Do not go too broad. "\
-                "the questions should not be 'WH' questions. "\
+                "the questions should not start with 'WH'. "\
                 "Give the answer in json array of strings. "\
-                "{questions: [...]}. "\
+                "['question1', 'question2', ...]. "\
                 "add nothing else to the answer. "\
                 "be a bit interactive."
-   
-def save_config(**kwargs):
+
+def save_config(config, **kwargs):
     for prop in kwargs:
         setattr(config, prop, kwargs[prop])
     config.save()
 
 def can_use():
+    config = ConfigDetails.get_config()
     now, last_used = datetime.now().timestamp(), config.last_used
     time_past = now - last_used
     usage = config.usage
     if datetime.fromtimestamp(last_used).date() != datetime.now().date():
         # reset usage: another day
-        save_config(usage=0)
+        save_config(config, usage=0)
     # used in the past 40 seconds or usage is okay for today
     if time_past < 40 or usage >= 40:
         return False
     # reset last_used as last called
-    save_config(last_used=datetime.now().timestamp(), usage=usage+1)
+    save_config(config, last_used=datetime.now().timestamp(), usage=usage+1)
     return True
     
 
 def questions_from_topic(topic, number):
-    return can_use() and interview_model.generate_content(
+    return can_use() and remove_outliers(interview_model.generate_content(
         f"Generate {number} questions on the topic '{topic}'" + more_queries
-    ).text
+    ).text)
     
 def questions_from_text(text, number):
-    return can_use() and interview_model.generate_content(
+    return can_use() and remove_outliers(interview_model.generate_content(
         f"Generate {number} questions on this text: {text}" + more_queries
-    ).text
+    ).text)
 
 
 def rewrite_all(questions, number):
-    if can_use():
-        various = interview_model.generate_content(
+    return can_use() and remove_outliers(interview_model.generate_content(
             f"Rewrite each question in {questions} in {number} different ways, keeping the meaning of each. "
             "none of your answers can be the same as the question I provide. "
             "return your answer in a json format. "
             ". whereby your return value is a list of lists containing strings, "
             f"and your return value's length is the same as that in {questions}"
-        ).text
-        return various[various.find('[') : various.rfind(']')+1]
-    return can_use()
+        ).text)
 
-
-#  test returns
-
-            # return HttpResponse(
-            #     json.dumps(
-            #         {"questions": [random() for i in range(number)]}
-            #         )
-            #     )  # testing
-
-
-            # return HttpResponse(
-            #     json.dumps(
-            #         [[random() for i in range(5)] for x in questions]
-            #         )
-            #     )
+def remove_outliers(text):
+    return text[text.find('[') : text.rfind(']')+1]
--- ai/models.py
+++ ai/models.py
@@ -1,14 +1,15 @@
 from django.db import models
 
-# Create your models here.
-
 class ConfigDetails(models.Model):
-    # {"last_used": 1719585805.504833, "usage": 14}
-    # I created an instance on the server and overrode the save method :)
+    # I was wrong to override the save method - now I can't really keep trak
+    # I should override it in only one condition
     last_used = models.FloatField(default=0)
     usage = models.IntegerField()
 
     def save(self):
-        pass
-
-config = ConfigDetails.objects.all()[0]  # Will change on Gemini query
+        if self == ConfigDetails.get_config():
+            super().save()
+    
+    @staticmethod
+    def get_config():
+        return ConfigDetails.objects.all()[0]
