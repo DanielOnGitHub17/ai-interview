ae7aeb763999d2b52ffaac40427f02b2e7af6f1c
Daniel Enesi <enesidaniel.120064@gmail.com>
2026-01-28 22:27:25 -0600

    Try new environment on vercel Dependency issue

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,12 @@
-python3 -m pip install -r requirements.txt
-python3 manage.py collectstatic
+# Create a virtual environment
+python3 -m venv venv
+
+# Activate it
+. venv/bin/activate
+
+# Install dependencies inside the venv
+pip install --upgrade pip
+pip install -r requirements.txt
+
+# Run collectstatic using the venv's python
+python manage.py collectstatic --noinput
